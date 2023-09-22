2f6bdd3bbb2c06f1ddc3a2150753a567e4a6fdfe
SvGaming <sven.kovac1@outlook.com>
2023-09-22 15:45:41 +0200

    improved usage, improved install

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
-echo "Please ensure you ran this script as a NORMAL USER, not as root (or with sudo)"
-echo "Installing..."
-install -D -t $HOME/bin bpkg
-chmod u+x $HOME/bin/bpkg
-install container-distros.txt $HOME/bin
-echo "export PATH=$HOME/bin:$PATH" >> $HOME/.bashrc
-echo "Install complete! Run exec bash or close and re-open your terminal to finish install!"
-
+if [ "$EUID" -eq 0 ]; then
+    echo -e "\033[01m\033[31m>> e: \033[39mplease run the installer as a non-root user\033[0m"
+    exit 1
+fi
+echo -e "\033[01m\033[36m>> i: \033[39minstalling\033[0m"
+install -D -t $HOME/.bin bpkg
+chmod u+x $HOME/.bin/bpkg
+echo -e "export PATH=$HOME/.bin:\$PATH" >> $HOME/.bashrc
+echo -e "\033[01m\033[36m>> i: \033[39minstall complete! run exec bash or close and re-open your terminal to finish install!\033[0m"
