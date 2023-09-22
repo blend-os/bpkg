84b20ea4b1ec48ae7e9b94f1bf7a13ae71b3be12
Asterisk <spacedude609@pm.me>
2023-09-22 13:28:20 +0000

    Remove container-distros.txt

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ echo "Please ensure you ran this script as a NORMAL USER, not as root (or with s
 echo "Installing..."
 install -D -t $HOME/bin bpkg
 chmod u+x $HOME/bin/bpkg
-install container-distros.txt $HOME/bin
 echo "export PATH=$HOME/bin:$PATH" >> $HOME/.bashrc
 echo "Install complete! Run exec bash or close and re-open your terminal to finish install!"
 
